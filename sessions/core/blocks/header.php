7ac4d17bc6ca9ead3f19b58155006b69803a82bf
I_am_weird <93086457+Andrew2507@users.noreply.github.com>
2024-08-13 23:42:28 +0300

    Delete sessions directory

--- sessions/core/blocks/header.php
+++
@@ -1,8 +0,0 @@
-<div class="header">
-    <div class="left">
-        <div class="name">ToDoForYou</div>
-        <div class="description">Организуйте свою жизнь здесь и сейчас.</i></div>
-    </div>
-
-    <div class="right"></div>
-</div>
